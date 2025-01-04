f23bc242025ba6cb05cd0397abdf1203f48a1adc
Mikhail <mkg0st@yandex.ru>
2025-01-04 16:43:22 +0300

    change testing initilization list of Projects to normal

--- ViewModel/ProgressVM.cs
+++ ViewModel/ProgressVM.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using Canvas.Database;
 using Canvas.Model;
 using Canvas.Model.ProjectModel;
 
@@ -29,55 +30,7 @@ public class ProgressVM : INotifyPropertyChanged
     {
         try
         {
-            Projects = new ObservableCollection<IProject>
-        {
-            new Project {Name = "Разработка нового продукта", Priority = "Высокий", Steps = new ObservableCollection<Step>
-            {
-                new (){StepName = "Закупка сырья", 
-                    Steps = new ObservableCollection<Step>
-                    {
-                        new (){StepName = "Полимер"},
-                        new (){StepName = "Наполнитель"},
-                        new (){StepName = "Добавка 1"},
-                        new (){StepName = "Добавка 2"},
-                        new (){StepName = "Добавка 3"},
-                    }}, 
-                new (){StepName = "Разработка рецептуры", Steps = new ObservableCollection<Step>
-                {
-                    new (){StepName = "Отработка 1"},
-                    new (){StepName = "Отработка 2"},
-                    new (){StepName = "Отработка 3"}
-                }}, 
-                new (){StepName = "Производство материала"}, 
-                new (){StepName = "Отчетные документы"}
-            }},
-            
-            
-            new ProjectWithStepsCategory {Name = "Отгрузка какого-то продукта", Priority = "Очень высокий", TemplateStepsCategory = new TemplateStepsCategory
-            {
-                CategoryName = "Проверка категории",
-                CategSteps = new ObservableCollection<Step>
-                {
-                    new () {StepName = "Закупка материалов"},
-                    new () {StepName = "Наработка продуктов"},
-                    new () {StepName = "Выходной контроль"},
-                    new () {StepName = "Оформление паспорта"},
-                    new () {StepName = "Отгрузка"}
-                }
-            }},
-            
-            
-            new Project {Name = "Договор", Priority = "Низкий", Steps = new ObservableCollection<Step>
-            {
-                new () {StepName = "Оформление договора"},
-                new () {StepName = "Составление ТЗ"},
-                new () {StepName = "Разработка материала"},
-                new () {StepName = "Оформление ТУ"},
-                new () {StepName = "Оформление тех. процесса"},
-                new () {StepName = "Завершение работы"}
-            }},
-
-        };
+            Projects = MainDatabase.GetAllProjects();
         }
         catch (Exception e)
         {
